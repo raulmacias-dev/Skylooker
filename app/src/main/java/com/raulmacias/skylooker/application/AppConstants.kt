96e80b79c8f703a599629da8e1dd8cdb93b6f564
Raúl Macías <raul.ma.tr@gmail.com>
2021-09-09 19:30:21 +0200

    Get Last Location

--- app/src/main/java/com/raulmacias/skylooker/application/AppConstants.kt
+++ app/src/main/java/com/raulmacias/skylooker/application/AppConstants.kt
@@ -6,4 +6,6 @@ object AppConstants {
     const val BASE_URL = "http://api.openweathermap.org/data/2.5/"
     const val BASE_URL_IMG = "http://openweathermap.org/img/wn/"
     const val API_KEY = "b9839d3ca58529508973d8e8ebd28150"
+
+    const val LOCATION_REQUEST = 1000
 }
--- app/src/main/java/com/raulmacias/skylooker/ui/forecast/ForecastFragment.kt
+++ app/src/main/java/com/raulmacias/skylooker/ui/forecast/ForecastFragment.kt
@@ -2,9 +2,9 @@ package com.raulmacias.skylooker.ui.forecast
 
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.pm.PackageManager
-import android.location.Location
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
 import android.util.Log
 import android.view.KeyEvent
@@ -16,9 +16,7 @@ import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import com.bumptech.glide.Glide
 import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
-import com.raulmacias.skylooker.R
 import com.raulmacias.skylooker.application.AppConstants
 import com.raulmacias.skylooker.application.Resource
 import com.raulmacias.skylooker.data.model.Forecast
@@ -31,7 +29,7 @@ import java.util.*
 import kotlin.math.roundToInt
 
 
-class ForecastFragment : Fragment(R.layout.fragment_forecast) {
+class ForecastFragment : Fragment(com.raulmacias.skylooker.R.layout.fragment_forecast) {
 
     private lateinit var binding: FragmentForecastBinding
     private lateinit var adapter: ForecastAdapter
@@ -39,56 +37,70 @@ class ForecastFragment : Fragment(R.layout.fragment_forecast) {
         ForecastViewModelFactory(ForecastRepoImpl(ForecastDataSource(RetrofitClient.webService)))
     }
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentForecastBinding.bind(view)
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        getLocation()
+
         binding.inputFindCityWeather.setOnKeyListener(View.OnKeyListener{ _, keyCode, event ->
             if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER){
-
-                viewModel.fetchWeather(city = binding.inputFindCityWeather.text.toString() ).observe(viewLifecycleOwner, Observer { result ->
-                    when(result){
-                        is Resource.Loading -> {
-                            binding.progressBar.visibility = View.VISIBLE
-                            binding.searchbg.visibility = View.VISIBLE
-                            binding.mainContainer.visibility = View.GONE
-                            Toast.makeText(context, "CARGANDO", Toast.LENGTH_SHORT).show()
-                        }
-                        is Resource.Error -> {
-                            binding.progressBar.visibility = View.GONE
-                            binding.searchbg.visibility = View.VISIBLE
-                            binding.mainContainer.visibility = View.GONE
-                            Toast.makeText(context, "CIUDAD NO EXISTE O NO SE PUEDO REALIZAR LA CONSULTA", Toast.LENGTH_SHORT).show()
-                        }
-                        is Resource.Success -> {
-
-                            result.data?.let { bindWeather(it) }
-
-                            viewModel.fetchForecast(city = binding.inputFindCityWeather.text.toString()).observe(viewLifecycleOwner, Observer { resultForecast ->
-                                when(resultForecast){
-                                    is Resource.Loading -> {
-                                        binding.progressBar.visibility = View.VISIBLE
-                                    }
-                                    is Resource.Error -> {
-                                        binding.progressBar.visibility = View.GONE
-                                    }
-                                    is Resource.Success -> {
-                                        binding.progressBar.visibility = View.GONE
-                                        adapter = ForecastAdapter(resultForecast.data!!.list)
-                                        binding.rvForecast.adapter = adapter
-                                        binding.inputFindCityWeather.text?.clear()
-                                    }
-                                }
-                            })
-                        }
-                    }
-                })
+                fetchData("")
             }
             false
         })
     }
 
+    private fun fetchData(locationName: String){
+
+        var location : String = if (locationName.isBlank()){
+            binding.inputFindCityWeather.text.toString()
+        }else{
+            locationName
+        }
+
+        viewModel.fetchWeather(city = location ).observe(viewLifecycleOwner, Observer { result ->
+            when(result){
+                is Resource.Loading -> {
+                    binding.progressBar.visibility = View.VISIBLE
+                    binding.searchbg.visibility = View.VISIBLE
+                    binding.mainContainer.visibility = View.GONE
+                    Toast.makeText(context, "CARGANDO", Toast.LENGTH_SHORT).show()
+                }
+                is Resource.Error -> {
+                    binding.progressBar.visibility = View.GONE
+                    binding.searchbg.visibility = View.VISIBLE
+                    binding.mainContainer.visibility = View.GONE
+                    Toast.makeText(context, "CIUDAD NO EXISTE O NO SE PUEDO REALIZAR LA CONSULTA", Toast.LENGTH_SHORT).show()
+                }
+                is Resource.Success -> {
+
+                    result.data?.let { bindWeather(it) }
+
+                    viewModel.fetchForecast(city = location).observe(viewLifecycleOwner, Observer { resultForecast ->
+                        when(resultForecast){
+                            is Resource.Loading -> {
+                                binding.progressBar.visibility = View.VISIBLE
+                            }
+                            is Resource.Error -> {
+                                binding.progressBar.visibility = View.GONE
+                            }
+                            is Resource.Success -> {
+                                binding.progressBar.visibility = View.GONE
+                                adapter = ForecastAdapter(resultForecast.data!!.list)
+                                binding.rvForecast.adapter = adapter
+                                binding.inputFindCityWeather.text?.clear()
+                            }
+                        }
+                    })
+                }
+            }
+        })
+    }
     private fun bindWeather(data: Forecast){
         binding.progressBar.visibility = View.GONE
         binding.searchbg.visibility = View.GONE
@@ -113,9 +125,68 @@ class ForecastFragment : Fragment(R.layout.fragment_forecast) {
     }
     private fun changeImageDetail(iconCode: String?){
         when (iconCode) {
-            "01d", "02d", "03d", "04d" -> binding.imageViewDetail.setImageResource(R.drawable.sunny_day)
-            "09d", "10d", "11d" -> binding.imageViewDetail.setImageResource(R.drawable.raining_day)
-            "13d", "50d" -> binding.imageViewDetail.setImageResource(R.drawable.snowfalling_day)
+            "01d", "02d", "03d", "04d" -> binding.imageViewDetail.setImageResource(com.raulmacias.skylooker.R.drawable.sunny_day)
+            "09d", "10d", "11d" -> binding.imageViewDetail.setImageResource(com.raulmacias.skylooker.R.drawable.raining_day)
+            "13d", "50d" -> binding.imageViewDetail.setImageResource(com.raulmacias.skylooker.R.drawable.snowfalling_day)
+        }
+    }
+
+    private fun getLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                AppConstants.LOCATION_REQUEST
+            )
+        } else {
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener(requireActivity()) { location ->
+                    if (location != null) {
+                        val geocoder = Geocoder(requireContext(), Locale.getDefault())
+                        var addresses: List<Address> = emptyList()
+                        addresses = geocoder.getFromLocation(
+                            location.latitude,
+                            location.longitude,
+                            // In this sample, we get just a single address.
+                            1)
+                        fetchData(addresses[0].locality)
+                        binding.textViewLocation.text = "Tú ubicación: ${addresses[0].locality}"
+                        Log.d("dev_", "adress: ${addresses[0].locality}")
+                    } else {
+                        binding.textViewLocation.text = "No encontrada última ubicación"
+                        Log.d("dev_", "not location")
+                    }
+                }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out kotlin.String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            AppConstants.LOCATION_REQUEST -> {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    getLocation()
+                } else {
+                    Toast.makeText(requireContext(), "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
